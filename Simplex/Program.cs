a91ae5dc0aafdbd3b2a19f08c4781af843f633b0
grimm004 <grimm004@gmail.com>
2018-01-10 22:20:58 +0000

    .

--- Simplex/Program.cs
+++ Simplex/Program.cs
@@ -7,11 +7,16 @@ namespace Simplex
         static void Main(string[] args)
         {
             LPParser parser = new LPParser();
-            //parser.SetObjectiveFunction(Console.ReadLine());
-            //parser.LinearProgram.ObjectiveFunction.Output();
-            parser.SetObjectiveFunction("8x + 10y + 7z");
-            parser.AddConstraint("x + 3y + 2z < 10");
-            parser.AddConstraint("x + 5y + z < 8");
+
+            //parser.SetObjectiveFunction("8x + 10y + 7z");
+            //parser.AddConstraint("x + 3y + 2z < 10");
+            //parser.AddConstraint("x + 5y + z < 8");
+
+            parser.SetObjectiveFunction("x + 2y - z");
+            parser.AddConstraint("2x + y + z < 14");
+            parser.AddConstraint("4x + 2y + 3z < 28");
+            parser.AddConstraint("2x + 5y + 5z < 30");
+
             parser.LinearProgram.Output();
             new Simplex(parser.LinearProgram).Solve();
             Console.ReadKey();
