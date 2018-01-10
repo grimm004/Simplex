1181b70f8ce84467ac86023de35ac4bd3b8fc2c6
grimm004 <grimm004@gmail.com>
2018-01-10 22:26:56 +0000

    Added examples

--- Simplex/Program.cs
+++ Simplex/Program.cs
@@ -6,20 +6,48 @@ namespace Simplex
     {
         static void Main(string[] args)
         {
+            LinearProgram lp = Example1();
+            lp.Output();
+            new Simplex(lp).Solve();
+            Console.ReadKey();
+        }
+
+        static LinearProgram Example1()
+        {
             LPParser parser = new LPParser();
+            parser.SetObjectiveFunction("8x + 10y + 7z");
+            parser.AddConstraint("x + 3y + 2z < 10");
+            parser.AddConstraint("x + 5y + z < 8");
+            return parser.LinearProgram;
+        }
 
-            //parser.SetObjectiveFunction("8x + 10y + 7z");
-            //parser.AddConstraint("x + 3y + 2z < 10");
-            //parser.AddConstraint("x + 5y + z < 8");
-
+        static LinearProgram Example2()
+        {
+            LPParser parser = new LPParser();
             parser.SetObjectiveFunction("x + 2y - z");
             parser.AddConstraint("2x + y + z < 14");
             parser.AddConstraint("4x + 2y + 3z < 28");
             parser.AddConstraint("2x + 5y + 5z < 30");
+            return parser.LinearProgram;
+        }
 
-            parser.LinearProgram.Output();
-            new Simplex(parser.LinearProgram).Solve();
-            Console.ReadKey();
+        static LinearProgram Example3()
+        {
+            LPParser parser = new LPParser();
+            parser.SetObjectiveFunction("3x + 4y");
+            parser.AddConstraint("x + y < 4");
+            parser.AddConstraint("2x + y < 5");
+            return parser.LinearProgram;
+        }
+
+        static LinearProgram Example4()
+        {
+            // TODO: FIX NEGITIVE ENTRIES
+            LPParser parser = new LPParser();
+            parser.SetObjectiveFunction("-2x + y");
+            parser.AddConstraint("x + 2y < 6");
+            parser.AddConstraint("3x + 2y < 12");
+            return parser.LinearProgram;
         }
     }
 }
